cf4d2f155c2b3749995113ebe566503fb885572d
anamaksimova <19cherrytree91@gmail.com>
2022-04-22 16:12:04 +0300

    git_lesson

---
+++ script/script.js
@@ -0,0 +1,12 @@
+'use scrict'
+const modal_title = document.querySelector('.modal__title');
+console.log('modal_title: ', modal_title);
+
+const modal_form = document.querySelector('.modal__form');
+console.log('modal_form: ', modal_form);
+
+const modal_checkbox_discount = document.querySelector('.modal__checkbox');
+console.log('modal_checkbox: ', modal_checkbox);
+
+const modal_input_discount = document.querySelector('.modal__input_discount');
+console.log('modal_input_discount: ', modal_input_discount);
